3b55d4968497e215f9d4db52049bc05c7309ee11
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-20 12:19:55 -0400

    Apply black format

--- test/test_example/test_example.py
+++ test/test_example/test_example.py
@@ -4,10 +4,11 @@ import unittest
 
 from pylib.example import example
 
-class TestExample(unittest.TestCase):
 
+class TestExample(unittest.TestCase):
     def test_example(self):
         example()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
