fb692da8e4421a834a8cacbd598c19eb9a68e071
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-20 12:12:00 -0400

    Drop unused script calls

--- style.sh
+++ style.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# enforce use of GNU version of coreutils
-. ./tidy/util/enforce_gnu_utils.sh
-
-# enforce availability of dependencies
-. ./tidy/util/enforce_dependency.sh sphinx-build
-
 SOURCE_HASH=$( find -path ./cpp/third-party -prune -false -o -type f | sort | xargs cat | sha1sum )
 
 isort .
