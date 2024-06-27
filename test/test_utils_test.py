30022bee4a33e38bd0eb630656dbca0ed8d64b37
Nanta_sp <nsophonrat@gmail.com>
2024-06-27 06:52:01 -0400

    chore: Add unit test for compare_text_to_pdf function

---
+++ test/test_utils_test.py
@@ -0,0 +1,17 @@
+import unittest
+from .script.utils_test import compare_text_to_pdf
+
+class TestUtilsTest(unittest.TestCase):
+
+    
+    def test_compare_text_to_pdf(self):
+        input_text = "Instead of requiring humans to manually derive rules and build models from analyzing large amounts"
+        pdf_path = "test/dummy_pdf/dummy1.pdf"
+        expected_result = True
+
+        result = compare_text_to_pdf(input_text, pdf_path)
+
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
