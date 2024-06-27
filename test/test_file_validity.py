26d866eb5379401038b76155f0406a1118066521
stkhuu <stkhuu@ucsd.edu>
2024-06-27 16:08:28 +0100

    updating pull request requirements
    reformatted from style.sh
    removed unused imports

--- test/test_file_validity.py
+++ test/test_file_validity.py
@@ -1,7 +1,7 @@
-import os
 import pytest
-from pypdf import PdfReader
+
 from script.cropPage import scaleCropPDFMargins
+
 
 @pytest.fixture
 def real_pdf_environment(tmp_path):
@@ -9,37 +9,40 @@ def real_pdf_environment(tmp_path):
     output_dir.mkdir()
 
     invalid_pdf_path = "/Users/stephaniekhuu/Documents/curaitor/test/PDFFiles/invalid_file.pdf"
-    valid_pdf_path = "/Users/stephaniekhuu/Documents/curaitor/test/PDFFiles/valid_file.pdf"
+    valid_pdf_path = (
+        "/Users/stephaniekhuu/Documents/curaitor/test/PDFFiles/valid_file.pdf"
+    )
 
     # Provide paths to the test functions
     return {
-        'invalid_pdf': str(invalid_pdf_path),
-        'valid_pdf': str(valid_pdf_path),
-        'output_dir': str(output_dir)
+        "invalid_pdf": str(invalid_pdf_path),
+        "valid_pdf": str(valid_pdf_path),
+        "output_dir": str(output_dir),
     }
 
 
 def test_valid_pdf_handling(real_pdf_environment):
-
     # Using real environment setup to get the paths
-    valid_pdf = real_pdf_environment['valid_pdf']
-    output_directory = real_pdf_environment['output_dir']
-    
+    valid_pdf = real_pdf_environment["valid_pdf"]
+    output_directory = real_pdf_environment["output_dir"]
+
     # Test should not raise an exception for a valid PDF
     try:
         scaleCropPDFMargins(valid_pdf, output_directory)
     except Exception as e:
-        pytest.fail(f"Function should not have raised an exception for a valid PDF: {str(e)}")
+        pytest.fail(
+            f"Function should not have raised an exception for a valid PDF: {str(e)}"
+        )
+
 
 def test_real_invalid_pdf_handling(real_pdf_environment):
     # Using real environment setup to get the paths
-    invalid_pdf = real_pdf_environment['invalid_pdf']
-    output_directory = real_pdf_environment['output_dir']
+    invalid_pdf = real_pdf_environment["invalid_pdf"]
+    output_directory = real_pdf_environment["output_dir"]
 
     # Expecting an exception due to invalid PDF
     with pytest.raises(Exception) as excinfo:
         scaleCropPDFMargins(invalid_pdf, output_directory)
-    
+
     # # Assert to check specific error message
     # assert "expected error message" in str(excinfo.value)
-
