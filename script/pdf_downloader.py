2cd8c55ba25f839a89c4332b6cdd751ad7c8c5af
mmore500 <mmore500@users.noreply.github.com>
2024-06-27 14:55:33 +0000

    Deploying to binder from @ mmore500/curaitor@5f7c6e8a03e4623722749471d8ad8a25c1b6384c 🚀

--- script/pdf_downloader.py
+++ script/pdf_downloader.py
@@ -2,22 +2,26 @@ import subprocess
 
 # Define the output folder
 output_folder = "/workspaces/curaitor/script/"
-project_id = "4udrf"  # Replace with your actual project ID; need to set the token
-username = "lizhenzhupearl@gmail.com" 
+project_id = (
+    "4udrf"  # Replace with your actual project ID; need to set the token
+)
+username = "lizhenzhupearl@gmail.com"
 
 # Construct the command with the output folder
 command = f"osf -p {project_id} -u {username} clone {output_folder}"
 
 
 # Running the command
-process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(
+    command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+)
 
 # Capture the output and errors
 stdout, stderr = process.communicate()
 
 # Decode the output and errors (assuming they are in bytes)
-stdout = stdout.decode('utf-8')
-stderr = stderr.decode('utf-8')
+stdout = stdout.decode("utf-8")
+stderr = stderr.decode("utf-8")
 
 # Print the output and errors
 print("Output:")
@@ -26,4 +30,3 @@ print(stdout)
 if stderr:
     print("Errors:")
     print(stderr)
-
