dd7ba29f1ceb316cd7526190c79d2f17bfc85e3f
lizhenzhupearl <lizhenzhupearl@users.noreply.github.com>
2024-06-28 10:16:08 +0000

    Deploying to binder from @ mmore500/curaitor@e25b13646ca42bbb8c94a715bc93814627fdce35 🚀

--- script/queryText.py
+++ script/queryText.py
@@ -140,8 +140,7 @@ def query_llm(query_text, prompt, llm):
         response_text = response[0]["generated_text"]
 
     elif llm.startswith("Ollama"):
-        # if llm == "Ollama-llama3":
-        chatbot = ChatOllama(model="llama3", temperature=0, temperature=0.0)
+        chatbot = ChatOllama(model="llama3", temperature=0)
         chat_prompt = ChatPromptTemplate.from_template(prompt_)
         chain = chat_prompt | chatbot | StrOutputParser()
         full_output = chain.invoke({"Context": {relevant_text}})
